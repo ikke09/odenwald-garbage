61d6611c94af95d033869a1d897c295fc082d340
ikke09 <development@fam-bechtluft.de>
2020-02-20 17:48:06 +0100

    Rename files with new naming schema

--- client/src/components/app/app.component.test.js
+++ client/src/components/app/app.component.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 
-import App from './app';
+import App from './app.component';
 
 let container = null;
 beforeEach(() => {
