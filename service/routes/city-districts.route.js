8e1a4cc26bbd2d49b19fb21d02c7167f1bad88c9
ikke09 <development@fam-bechtluft.de>
2020-02-26 17:32:47 +0100

    Added additional route to get only specific districts for a city

--- service/routes/city-districts.route.js
+++ service/routes/city-districts.route.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.json(cityDistricts);
 });
 
+router.get('/:city', function(req, res, next) {
+  res.json(cityDistricts[req.params['city']]);
+});
+
 module.exports = router;
