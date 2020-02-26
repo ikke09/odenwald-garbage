7a3b1d1e4eecb0ffa10c7fd907de6c98e47c8cd9
ikke09 <development@fam-bechtluft.de>
2020-02-26 17:33:10 +0100

    Implementing supertests for all routes

---
+++ service/routes/city-district.route.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const app = require('../app');
+const cityDistricts = require('../src/constants/city-districts');
+
+describe('City-District Route', () => {
+  it('/api/citydistricts should return all cities with their districts', (done) => {
+    return request(app)
+      .get('/api/citydistricts')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toStrictEqual(cityDistricts);
+        done();
+      });
+  });
+  it('/api/citydistricts/Michelstadt should return all districts for Michelstadt', (done) => {
+    return request(app)
+      .get('/api/citydistricts/Michelstadt')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toStrictEqual(cityDistricts['Michelstadt']);
+        done();
+      });
+  });
+});
