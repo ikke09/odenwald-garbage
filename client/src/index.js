6e5e9963b0d394f496b9ebbec2bf1a38ea32fbe0
ikke09 <development@fam-bechtluft.de>
2020-03-06 21:49:04 +0100

    Adding dotenv package with template

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app.component';
+const config = require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
