b24e6aabfe1def942eb5611b74cb0855a4941f94
ikke09 <development@fam-bechtluft.de>
2020-03-13 07:57:33 +0100

    Remove dotenv import

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import moment from 'moment';
 import 'moment/locale/de';
 import './index.css';
 import App from './components/App/App';
-const config = require('dotenv').config();
 
 moment.locale('de');
 ReactDOM.render(<App />, document.getElementById('root'));
