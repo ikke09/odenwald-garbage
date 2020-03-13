6326d440555e88f9bdf5fbfe796aa21c5137cab2
ikke09 <development@fam-bechtluft.de>
2020-03-13 07:57:01 +0100

    Implement DropDown-Component

--- client/src/components/DropDown/DropDown.jsx
+++ client/src/components/DropDown/DropDown.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Select, MenuItem } from '@material-ui/core';
 import './DropDown.css';
 
-const DropDown = (props) => {
+const DropDown = ({ name, value, default: predefined, options, onChange }) => {
+  const handleChange = (event) => {
+    const { value } = event.target;
+    onChange(value);
+  };
+
+  console.log('Rendering DropDown', name);
+
   return (
-    <div>
-      <span>DropDown</span>
-    </div>
+    <Select name={name} autoWidth value={value || predefined} onChange={handleChange}>
+      {options.map((opt, index) => (
+        <MenuItem key={index} value={opt}>
+          {opt}
+        </MenuItem>
+      ))}
+    </Select>
   );
+};
+
+DropDown.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  default: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onChange: PropTypes.func.isRequired
 };
 
 export default DropDown;
