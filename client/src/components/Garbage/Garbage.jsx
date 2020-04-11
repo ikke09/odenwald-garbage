f71cf2c6e802aacd79de5e74356eb1b49f2e589c
ikke09 <development@fam-bechtluft.de>
2020-04-11 14:38:16 +0200

    Garbage styling with cards

--- client/src/components/Garbage/Garbage.jsx
+++ client/src/components/Garbage/Garbage.jsx
@@ -1,16 +1,70 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { styled } from '@material-ui/core/styles';
+import { Card, CardContent, Grid, Divider, Box } from '@material-ui/core';
 
-const Garbage = (props) => {
-  console.log('Render Garbage', props);
-  if (!props.data) return null;
-  const garbages = props.data;
+const GarbageContainer = styled(Grid)({
+  width: '100%',
+  'flex-direction': 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+const GarbageCard = styled(Card)({
+  borderRadius: 12,
+  minWidth: 256,
+  minHeight: 128,
+  textAlign: 'center',
+});
+
+const GarbageCardHeader = styled('h3')({
+  fontSize: 18,
+  fontWeight: 'bold',
+  letterSpacing: '0.5px',
+  marginTop: 4,
+  marginBottom: 4,
+});
+
+const GarbageCardContentBox = styled('div')({
+  backgroundColor: (props) => props.bg,
+});
+
+const Garbage = ({ garbages }) => {
+  console.log('Render Garbage', garbages);
+  if (!garbages || garbages.length === 0) {
+    return (
+      <GarbageContainer container>
+        <span>Keine Müllabholung</span>
+      </GarbageContainer>
+    );
+  }
   return (
-    <div>
-      {garbages.map((g) => (
-        <p key={g.garbageBin.shortName}>{g.garbageBin.fullName}</p>
+    <GarbageContainer container spacing={3}>
+      {garbages.map((garbage) => (
+        <Grid key={garbage.garbageBin.shortName} item>
+          <GarbageCard variant='outlined'>
+            <CardContent>
+              <GarbageCardHeader>{garbage.garbageBin.fullName}</GarbageCardHeader>
+              <Divider light />
+              <GarbageCardContentBox bg={garbage.garbageBin.color}></GarbageCardContentBox>
+            </CardContent>
+          </GarbageCard>
+        </Grid>
       ))}
-    </div>
+    </GarbageContainer>
   );
+};
+
+Garbage.propTypes = {
+  garbages: PropTypes.arrayOf(
+    PropTypes.shape({
+      garbageBin: PropTypes.shape({
+        shortName: PropTypes.string.isRequired,
+        fullName: PropTypes.string.isRequired,
+        color: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired
+  ).isRequired,
 };
 
 export default Garbage;
