d50d6a44775e600b97442014ffeab3bfe0f479f1
ikke09 <development@fam-bechtluft.de>
2020-05-29 07:33:51 +0200

    Cache garbages request data

--- functions/garbages.js
+++ functions/garbages.js
@@ -1,6 +1,12 @@
 const scraper = require('./garbages/scrape');
 const moment = require('moment');
 
+const cache = {
+  threshhold: 86400000, // Day as milliseconds
+  lastFetch: 0,
+  garbageDictionary: {}
+};
+
 function parsePath(path) {
   let url = decodeURI(path);
   const paramsRegex = new RegExp(/\/\.netlify\/functions\/garbages\/([\wä-ü-ß]+)\/([\wä-ü-ß]+)\/?(\d+\-\d+)?/, 'sg');
@@ -14,14 +20,28 @@ function loadGarbages({ city, district, day = null }, callback) {
   if (!city || !district) {
     throw new Error('city & district can not be null!');
   }
-  scraper.scrape(city, district, moment().year(), (data) => {
-    let result = data;
-    if (day && data) {
-      const queryDay = moment(day, 'DD-MM');
-      result = data.filter((d) => d.date.isSame(queryDay, 'day'));
-    }
-    callback(result);
-  });
+
+  const timeSinceLastFetch = Date.now() - cache.lastFetch;
+  const dictionaryKey = `${city}-${district}`;
+  if (timeSinceLastFetch <= cache.threshhold && !!cache.garbageDictionary[dictionaryKey]) {
+    const data = cache.garbageDictionary[dictionaryKey];
+    callback(filterGarbages(data, day));
+  } else {
+    scraper.scrape(city, district, moment().year(), (data) => {
+      cache.garbageDictionary[dictionaryKey] = data;
+      cache.lastFetch = Date.now();
+      callback(filterGarbages(data, day));
+    });
+  }
+}
+
+function filterGarbages(data, day) {
+  let result = data;
+  if (day && data) {
+    const queryDay = moment(day, 'DD-MM');
+    result = data.filter((d) => d.date.isSame(queryDay, 'day'));
+  }
+  return result;
 }
 
 exports.handler = function (event, context, callback) {
